2c69ce6c96700b027150ca744eabd43c869f5f41
MrKopustor <zotop1753@yahoo.com>
2024-04-14 19:39:13 +0300

    added task_1

---
+++ src/main/java/com/teachmeskills/lesson21/hw/Main.java
@@ -0,0 +1,14 @@
+package com.teachmeskills.lesson21.hw;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Start programs");
+        log.error("F");
+        System.out.println("Hi!");
+
+        log.info("End of program");
+    }
+}
